c7396fbb2074c6ed6909559d7bf2dff9a89f9290
ck-nebel <erick.p.camargo@gmail.com>
2026-06-03 18:51:17 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors({
   origin: 'https://organizador-de-leituras-frontend.vercel.app' // Depois do deploy, troque pelo domínio exato do seu frontend
 }));
+app.options('*', cors());
 
 app.use(express.json());
 
